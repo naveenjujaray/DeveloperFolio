1e1eabcfeb3251f0f145c65f324c57f8c160eae5
Jujaray Naveen <jujaraynaveen@gmail.com>
2021-07-16 23:34:47 +0530

    added education

--- lib/include/Rows/educationmain.dart
+++ lib/include/Rows/educationmain.dart
@@ -50,13 +50,104 @@ class EduDesk extends StatelessWidget {
                  Text("2017 - 2021", 
                 style: TextStyle(fontSize: 22,),
                 ),
-                Text("•  Volunteered in NSS for two years", 
+                Text("•  Participated in Rural Development and Social Events as Student Volunteer in NSS for 2 years", 
                 style: TextStyle(fontSize: 22, color: Colors.grey),
                 ),
                   ],
                 )
             ],
           ),
+           SizedBox(height: 30,),
+          Row(
+            children: [
+              SizedBox(width: 15,),
+              Container(
+                  height: 150,
+                  width: 150,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    boxShadow: [new BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 15.0,
+                        spreadRadius: 5.0,
+                        offset: Offset(
+                          0.0, // Move to right 10  horizontally
+                          10.0, // Move to bottom 10 Vertically
+              ),
+ 
+                    ),],
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/narayana.png'),
+                    ),
+                  ),
+                  //child: Image.asset('assets/images/gitam.png'),
+               ),
+               SizedBox(width: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Narayana Junior College, Hyderabad", 
+                style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),
+                ),
+                 Text("Board of Intermediate Education Telangana", 
+                style: TextStyle(fontSize: 26,fontWeight: FontWeight.w600),
+                ),
+                 Text("2015 - 2017", 
+                style: TextStyle(fontSize: 22,),
+                ),
+               
+                  ],
+                )
+            ],
+          ),
+          SizedBox(height: 30,),
+          Row(
+            children: [
+              SizedBox(width: 15,),
+              Container(
+                  height: 150,
+                  width: 150,
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    boxShadow: [new BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 15.0,
+                        spreadRadius: 5.0,
+                        offset: Offset(
+                          0.0, // Move to right 10  horizontally
+                          10.0, // Move to bottom 10 Vertically
+              ),
+ 
+                    ),],
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/school.png'),
+                    ),
+                  ),
+                  //child: Image.asset('assets/images/gitam.png'),
+               ),
+               SizedBox(width: 20,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("St. John's High School, Karimnagar", 
+                style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),
+                ),
+                 Text("Board of Secondary Education Telangana", 
+                style: TextStyle(fontSize: 26,fontWeight: FontWeight.w600),
+                ),
+                 Text("2015", 
+                style: TextStyle(fontSize: 22,),
+                ),
+               
+                  ],
+                )
+            ],
+          ),
+                  SizedBox(height: 30,),
         ],
       ),
     );
@@ -126,13 +217,127 @@ class EduTab extends StatelessWidget {
                      Text("2017 - 2021", 
                     style: TextStyle(fontSize: 22,),
                     ),
-                    Text("•  Volunteered in NSS for two years", 
+                    Text("•  Participated in Rural Development and Social Events as Student Volunteer in NSS for 2 years", 
                     style: TextStyle(fontSize: 22, color: Colors.grey),
                     ),
                       ],
                     ),
                               ),
-                              SizedBox(height: 20,)
+                              SizedBox(height: 20,),
+                              //Narayana
+                  SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+                      child: Column(
+              
+                children: [
+                  SizedBox(width: 15,),
+                  Center(
+                    child: Container(
+                        margin: EdgeInsets.all(10.0),
+                        padding: EdgeInsets.all(10.0),
+                          height: 150,
+                          width: 150,
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                            
+            boxShadow: [new BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(
+                      0.0, // Move to right 10  horizontally
+                      10.0, // Move to bottom 10 Vertically
+                      ), 
+            ),],
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/narayana.png'),
+            ),
+                          ),
+                          //child: Image.asset('assets/images/gitam.png'),
+                        
+                       ),
+                  ),
+                    ],
+              
+              ),
+          ),
+           Center(
+                                child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+            Text("Narayana Junior College, Hyderabad", 
+                    style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold),
+                    ),
+                     Text("Intermediate in MPC stream", 
+                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),
+                    ),
+                     Text("2015 - 2017", 
+                    style: TextStyle(fontSize: 22,),
+                    ),
+                    
+                      ],
+                    ),
+                              ),
+                              SizedBox(height: 20,),
+                              //SCHOOL
+                  SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+                      child: Column(
+              
+                children: [
+                  SizedBox(width: 15,),
+                  Center(
+                    child: Container(
+                        margin: EdgeInsets.all(10.0),
+                        padding: EdgeInsets.all(10.0),
+                          height: 150,
+                          width: 150,
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                            
+            boxShadow: [new BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(
+                      0.0, // Move to right 10  horizontally
+                      10.0, // Move to bottom 10 Vertically
+                      ), 
+            ),],
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/school.png'),
+            ),
+                          ),
+                          //child: Image.asset('assets/images/gitam.png'),
+                        
+                       ),
+                  ),
+                    ],
+              
+              ),
+          ),
+           Center(
+                                child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+            Text("St. John's High School, Karimnagar", 
+                    style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold),
+                    ),
+                     Text("Board of Secondary Education Telangana", 
+                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),
+                    ),
+                     Text("2015", 
+                    style: TextStyle(fontSize: 22,),
+                    ),
+                    
+                      ],
+                    ),
+                              ),
+                              SizedBox(height: 20,),                      
         ],        
       ),
       ),
@@ -203,13 +408,127 @@ class EduMob extends StatelessWidget {
                        Text("2017 - 2021", 
                       style: TextStyle(fontSize: 17,),
                       ),
-                      Text("•  Volunteered in NSS for two years", 
+                      Text("•  Participated in Rural Development and Social Events as Student Volunteer in NSS for 2 years", 
                       style: TextStyle(fontSize: 17, color: Colors.grey),
                       ),
                         ],
                       ),
            ),
-           SizedBox(height: 20,)
+           SizedBox(height: 20,),
+            SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+                      child: Center(
+                        child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(5.0),
+                            height: 100,
+                            width: 100,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              
+            boxShadow: [new BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(
+                      0.0, // Move to right 10  horizontally
+                      10.0, // Move to bottom 10 Vertically
+                      ), 
+            ),],
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/narayana.png'),
+            ),
+                            ),
+                            //child: Image.asset('assets/images/gitam.png'),
+                          
+                       ),
+                  ),
+                    ],
+              
+              ),
+                      ),
+          ),
+           Center(
+             child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+              Text("Narayana Junior College, Hyderabad", 
+                      style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
+                      ),
+                       Text("Board of Intermediate Education Telangana", 
+                      style: TextStyle(fontSize: 18,fontWeight: FontWeight.w600),
+                      ),
+                       Text("2015 - 2017", 
+                      style: TextStyle(fontSize: 17,),
+                      ),
+                     
+                        ],
+                      ),
+           ),
+           SizedBox(height: 20,),
+                    SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+                      child: Center(
+                        child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(5.0),
+                            height: 100,
+                            width: 100,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              
+            boxShadow: [new BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10.0,
+                spreadRadius: 5.0,
+                offset: Offset(
+                      0.0, // Move to right 10  horizontally
+                      10.0, // Move to bottom 10 Vertically
+                      ), 
+            ),],
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/school.png'),
+            ),
+                            ),
+                            //child: Image.asset('assets/images/gitam.png'),
+                          
+                       ),
+                  ),
+                    ],
+              
+              ),
+                      ),
+          ),
+           Center(
+             child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+              Text("St. John's High School, Karimnagar", 
+                      style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
+                      ),
+                       Text("Board of Secondary Education Telangana", 
+                      style: TextStyle(fontSize: 18,fontWeight: FontWeight.w600),
+                      ),
+                       Text("2015", 
+                      style: TextStyle(fontSize: 17,),
+                      ),
+                     
+                        ],
+                      ),
+           ),
+           SizedBox(height: 20,),
         ],
       ),
     );
